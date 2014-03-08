75904e8d23cf42925ed33e543ab883f537229f08
PabloIndio <indien-skuall@hotmail.es>
2014-03-08 01:09:56 +0100

    Añadido crear en cursor
    Script para crear un prefab en la posición en al que se hace click

---
+++ Assets/Scripts/CrearEnCursor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+// Crea un prefab en la posicion del cursor al hacer click
+
+public class CrearEnCursor : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Transform objetoAColocar;
+	private Ray ray;
+
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		bool shoot = Input.GetButtonDown("Fire1");
+
+		if (shoot) {
+
+			var shotTransform = Instantiate(objetoAColocar) as Transform;
+
+
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition );
+			mousePos.z = 0;
+	
+			shotTransform.position= mousePos;
+
+
+
+		}
+	
+	}
+}
